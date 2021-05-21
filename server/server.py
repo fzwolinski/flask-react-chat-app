730f1f662b12285c0bb2ec2c8896b404af4829cc
Fabian Zwolinski <Giovacho@tuta.io>
2021-05-21 18:37:27 +0200

    Add very simple flask server

---
+++ server/server.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "somesecret"
+
+socket = SocketIO(app)
+
+
+@socket.on('message')
+def handle_message(msg):
+  print(f"[Message]: {msg}")
+  send(msg, broadcast = True)
+
+
+if __name__ == "__main__":
+  socket.run(app)
