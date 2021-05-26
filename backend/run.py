14782f6b018fc379ec5e371fc810e9a6ef5a848c
Fabian Zwolinski <Giovacho@tuta.io>
2021-05-26 19:44:57 +0200

    Deployment changes and additions

--- backend/run.py
+++ backend/run.py
@@ -2,4 +2,4 @@ from server import app, socket, db
 
 if __name__ == "__main__":
   db.create_all()
-  socket.run(app, debug=True)
+  socket.run(app, port=5000, debug=False)
--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
@@ -8,10 +9,12 @@ app.config['SECRET_KEY'] = "somesecret"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 db = SQLAlchemy(app)
 
 socket = SocketIO(app, cors_allowed_origins="*")
 
 from server import models
 from server import sockets
-
